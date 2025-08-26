4f714d11d82f7bba53861c0904531a363b1dbb81
Nobita <58627889+Nobita22@users.noreply.github.com>
2025-08-27 00:21:40 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@ if (workbox) {
   // Precache files with explicit revision to ensure update on change
   workbox.precaching.precacheAndRoute([
     { url: '/pi//index.html', revision: '4' }, // Increment revision on file update
-    { url: '/pi/manifest.json', revision: '2' },
+    { url: '/pi/manifest.json', revision: '3' },
     { url: '/pi/icon2.png', revision: '2' },
     // Add other assets here with revisioning as needed
   ]);
